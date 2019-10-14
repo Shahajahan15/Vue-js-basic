0273762d2265ccf07cdd04e742e2326aab1d092e
Shahajahan15 <mdrezaali1@gmail.com>
2019-10-14 12:51:32 +0600

    Data bindings

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 
 
-	var result = new Vue({
+	var app = new Vue({
 		el: '#root',
 		data: {
-			message: "Hello World.",
+			name: "Shahajahan",
 		},
+		mounted: function () {
+			console.log("mounted")
+		}
 
 	})
+
+
+/*	// javascript 
+	var data = {name:"Safikul"}
+	document.getElementById("name").value = data.name;
+	document.getElementById("output").innerHTML = "Your name is : " + data.name;
+
+	function change() {
+		data.name = document.getElementById("name").value;
+		document.getElementById("output").innerHTML = "Your name is : " + data.name;
+	}*/
+	
 

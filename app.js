23385ce54e44bd5095b09f27ee6660929efacef9
Shahajahan15 <mdrezaali1@gmail.com>
2019-10-16 22:57:25 +0600

    Filters to UpperCase, LowerCase, date format with local and global felter

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 
 
+	//  global filters
+	Vue.filter("lower", function(s){
+		return s.toLowerCase()
+	})
+
 	var app = new Vue({
 		el: '#root',
 		data: {
-			payments: [
-				{name: "houseRent", amount: 1500, paid: true},
-				{name: "electricity", amount: 1000, paid: false},
-				{name: "gas", amount: 800, paid: true},
-				{name: "water", amount: 700, paid: false},
-				{name: "net", amount: 600, paid: true},
-			]
+			name: "Shahajahan All Cse",
+			profession: "Software Engineering",
+			date: new Date()
 		},
 		computed: {
-			unpaidPayments(){
-				return this.payments.filter(function(p){
-					return !p.paid;
-				})
+			upperCaseName(){
+				return this.name.toUpperCase()
+			}
+		},
+		//  locally filters
+		filters: {  
+			upper(s){
+				return s.toUpperCase()
 			},
 
-			totalAmount(){
-				var sum = 0
-				for (var i = 0; i < this.payments.length; i++) {
-					sum += this.payments[i].amount
-				}
-				return sum
+			bdformat(d){
+				return d.getUTCDate() + "-" + (d.getUTCMonth()+1) + "-" + d.getUTCFullYear()
 			}
 		}
 	})
+
+
+
+
+
 
 
 

d79f6dea65edf019a17beef11b8002729b3ca35b
Shahajahan15 <mdrezaali1@gmail.com>
2019-10-17 15:34:29 +0600

    components to props, slot, condition to view and hide

--- app.js
+++ app.js
@@ -1,39 +1,64 @@
 
-
-	//  global filters
-	Vue.filter("lower", function(s){
-		return s.toLowerCase()
-	})
-
-	var app = new Vue({
-		el: '#root',
-		data: {
-			name: "Shahajahan All Cse",
-			profession: "Software Engineering",
-			date: new Date()
-		},
-		computed: {
-			upperCaseName(){
-				return this.name.toUpperCase()
-			}
-		},
-		//  locally filters
-		filters: {  
-			upper(s){
-				return s.toUpperCase()
-			},
-
-			bdformat(d){
-				return d.getUTCDate() + "-" + (d.getUTCMonth()+1) + "-" + d.getUTCFullYear()
+	
+	// component
+	Vue.component("pannel", {
+		template: `
+			<div class="pannel" v-if="show">  
+				<div class="pannelTitle">
+					<div class="close" @click="show = false">x</div>
+					{{ title }}
+				</div>
+				<div class="pannelContent">
+					{{ content }}
+				</div>
+				<div class="inputs">
+					<slot></slot>
+				</div>
+			</div>
+		`,
+		props:['title', 'content'],
+		data: function(){
+			return {
+				show: true
 			}
 		}
 	})
 
+	var app = new Vue({
+		el: "#root",
+		data: {
 
+		}
+	})
 
+	//  global filters
+	// Vue.filter("lower", function(s){
+	// 	return s.toLowerCase()
+	// })
 
+	// var app = new Vue({
+	// 	el: '#root',
+	// 	data: {
+	// 		name: "Shahajahan All Cse",
+	// 		profession: "Software Engineering",
+	// 		date: new Date()
+	// 	},
+	// 	computed: {
+	// 		upperCaseName(){
+	// 			return this.name.toUpperCase()
+	// 		}
+	// 	},
+	// 	//  locally filters
+	// 	filters: {  
+	// 		upper(s){
+	// 			return s.toUpperCase()
+	// 		},
 
-
+	// 		bdformat(d){
+	// 			return d.getUTCDate() + "-" + (d.getUTCMonth()+1) + "-" + d.getUTCFullYear()
+	// 		}
+	// 	}
+	// })
 
 
 /*	// javascript 
